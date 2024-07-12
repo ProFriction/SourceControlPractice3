b8c3febc27cfd25547b4794254affc01c20a22ce
Alex <alexander.e.j.charles@gmail.com>
2024-07-12 13:32:00 -0400

    Initial Commit

---
+++ SourceControlPractice/SourceControlPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlPracticeApp.swift
+//  SourceControlPractice
+//
+//  Created by Alex Charles on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
